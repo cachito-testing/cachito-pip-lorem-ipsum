bb3a75f4c5b6fc5cf2ffb9c6c147d580458c6c48
Adam Cmiel <acmiel@redhat.com>
2020-09-22 15:19:00 +0200

    Remove build dependencies

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ import setuptools
 setuptools.setup(
     name="lorem-ipsum",
     version="0.1.0",
-    setup_requires=["setuptools", "wheel"],
     install_requires=["requests"],
     packages=setuptools.find_packages(),
     entry_points={"console_scripts": ["lorem.py = lorem_ipsum.lorem:main"]},
